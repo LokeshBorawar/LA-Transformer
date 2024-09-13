e5ec3b56cf54e52b639de1c3fe0b2a94c79ee6b4
Lokesh Borawar <borawarlokesh26@gmail.com>
2024-09-13 12:39:29 +0530

    Add update for twin2.

--- Video_to_gif.py
+++ Video_to_gif.py
@@ -3,6 +3,6 @@ from moviepy.editor import VideoFileClip
 # Load the video file
 clip = VideoFileClip("Video/Out/twin.mp4")
 # Extract the segment from 3 to 9 seconds
-clip = clip.subclip(3, 9)
+clip = clip.subclip(29, 35)
 # Convert to gif
-clip.write_gif("Video/Out/twin.gif")
+clip.write_gif("Video/Out/twin2.gif")
